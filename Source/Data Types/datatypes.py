56cbd772fb1bc8b77b0a1dd4d2cfeb71eabaaace
jojolapa2github <jojolapaNepal@gmail.com>
2024-05-13 21:57:29 -0700

    Adding print and input syntax

---
+++ Source/Data Types/datatypes.py
@@ -0,0 +1,12 @@
+print("Print in python programming***************************************")
+x = 5
+y = 12.5
+print("Sum of", x, "and", y, "equals", x + y)
+print("Sum of {} and {} equals".format(x, y), x + y)
+print("Sum of %d and %d equals" % (x, y), x + y)
+print(f"Sum of {x} and {y} equals", x + y)
+
+print("Input in python programming***************************************")
+x = int(input('Type value of x:'))
+y = float(input('Type value of y:2'))
+print(f"Sum of {x} and {y} equals", x + y)
