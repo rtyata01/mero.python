d51744f07a76feaa6e993eae2dd71336d56c81de
jojolapa2github <jojolapaNepal@gmail.com>
2024-05-31 13:36:48 -0700

    sample OpenAI API integration calls.

---
+++ Source/OpenAi/openai_setup.py
@@ -0,0 +1,8 @@
+# Setup the virtual enviornment optional https://platform.openai.com/docs/quickstart
+#python -m venv openai-env
+
+#Activate the openai for Windows Operating sytem
+#openai-env\Scripts\activate
+
+#Install the openai python library
+#pip install --upgrade openai
